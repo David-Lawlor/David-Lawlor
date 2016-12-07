9e5d6ae7774f2c7299341c0ea5483243263d72a8
David <lawlor041988@gmail.com>
2016-12-07 23:14:03 +0000

    moved styles from page to separate files. pushing for testing

---
+++ js/circliful-rings.js
@@ -0,0 +1,129 @@
+function myFunction() {
+                $("#test-circle1").html("");
+                $("#test-circle1").circliful({
+                    animation: 1,
+                    animationStep: 5,
+                    foregroundBorderWidth: 15,
+                    backgroundBorderWidth: 15,
+                    percent: 82,
+                    textSize: 28,
+                    textStyle: 'font-size: 24px;',
+                    textColor: '#666',
+                    multiPercentage: 1,
+                    percentages: [10, 20, 30],
+                    text: "Java", 
+                    textBelow: true
+                });
+
+                $("#test-circle2").html("");
+                $("#test-circle2").circliful({
+                    animation: 1,
+                    animationStep: 5,
+                    foregroundBorderWidth: 15,
+                    backgroundBorderWidth: 15,
+                    percent: 55,
+                    textSize: 28,
+                    textStyle: 'font-size: 24px;',
+                    textColor: '#666',
+                    multiPercentage: 1,
+                    percentages: [10, 20, 30],
+                    text: "C#", 
+                    textBelow: true
+                }); 
+
+                $("#test-circle3").html("");
+                $("#test-circle3").circliful({
+                    animation: 1,
+                    animationStep: 5,
+                    foregroundBorderWidth: 15,
+                    backgroundBorderWidth: 15,
+                    percent: 60,
+                    textSize: 28,
+                    textStyle: 'font-size: 24px;',
+                    textColor: '#666',
+                    multiPercentage: 1,
+                    percentages: [10, 20, 30],
+                    text: "C++", 
+                    textBelow: true
+                }); 
+
+                $("#test-circle4").html("");
+                $("#test-circle4").circliful({
+                    animation: 1,
+                    animationStep: 5,
+                    foregroundBorderWidth: 15,
+                    backgroundBorderWidth: 15,
+                    percent: 72,
+                    textSize: 28,
+                    textStyle: 'font-size: 24px;',
+                    textColor: '#666',
+                    multiPercentage: 1,
+                    percentages: [10, 20, 30],
+                    text: "SQL", 
+                    textBelow: true
+                }); 
+
+                $("#test-circle5").html("");
+                $("#test-circle5").circliful({
+                    animation: 1,
+                    animationStep: 5,
+                    foregroundBorderWidth: 15,
+                    backgroundBorderWidth: 15,
+                    percent: 73,
+                    textSize: 28,
+                    textStyle: 'font-size: 24px;',
+                    textColor: '#666',
+                    multiPercentage: 1,
+                    percentages: [10, 20, 30],
+                    text: "HTML", 
+                    textBelow: true
+                }); 
+
+                $("#test-circle6").html("");
+                $("#test-circle6").circliful({
+                    animation: 1,
+                    animationStep: 5,
+                    foregroundBorderWidth: 15,
+                    backgroundBorderWidth: 15,
+                    percent: 67,
+                    textSize: 28,
+                    textStyle: 'font-size: 24px;',
+                    textColor: '#666',
+                    multiPercentage: 1,
+                    percentages: [10, 20, 30],
+                    text: "CSS", 
+                    textBelow: true
+                }); 
+
+                $("#test-circle7").html("");
+                $("#test-circle7").circliful({
+                    animation: 1,
+                    animationStep: 5,
+                    foregroundBorderWidth: 15,
+                    backgroundBorderWidth: 15,
+                    percent: 30,
+                    textSize: 28,
+                    textStyle: 'font-size: 24px;',
+                    textColor: '#666',
+                    multiPercentage: 1,
+                    percentages: [10, 20, 30],
+                    text: "Javascript", 
+                    textBelow: true
+                }); 
+
+                $("#test-circle8").html("");
+                $("#test-circle8").circliful({
+                    animation: 1,
+                    animationStep: 5,
+                    foregroundBorderWidth: 15,
+                    backgroundBorderWidth: 15,
+                    percent: 80,
+                    textSize: 28,
+                    textStyle: 'font-size: 24px;',
+                    textColor: '#666',
+                    multiPercentage: 1,
+                    percentages: [10, 20, 30],
+                    text: "Linux", 
+                    textBelow: true
+                }); 
+            }
